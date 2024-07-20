23bfe7cfc821aee20367fab2eb0b549f21c530f5
Playbackend <backend@playdate.africa>
2024-07-20 23:08:32 +0100

    Changes to be committed: 	modified:   src/components/LandingPage/SectionFive.jsx 	modified:   src/components/LandingPage/Sectiontwo.jsx

--- src/components/LandingPage/SectionFive.jsx
+++ src/components/LandingPage/SectionFive.jsx
@@ -11,29 +11,29 @@ import orangebg from "../../../public/orangebg.png";
 
 
 const SectionFive = () => {
-  return (
-    <main className={`mx-auto flex mt-40 mb-10   ${inter.className}`}>
-      {/* container */}
-      <div className="flex rounded-[40px] w-[1340px] h-[440px] mx-auto" >
-        <Image src={orangebg} width={1213} height={440} className="h-[440px] absolute  w-[1213px]" />
+    return (
+      <main className={`mx-auto flex mt-40 mb-10   ${inter.className}`}>
+        {/* container */}
+        <div className="flex rounded-[40px] w-[1340px] h-[440px] mx-auto" >
+        <Image src={orangebg} width={1213} height={440} className="h-[440px] absolute  w-[1213px]"/>
         <div className="w-[1213px] h-[440px] flex flex-row   relative   justify-between">
           {/* 01 */}
           <div className="w-full justify-center mx-20 my-auto">
-            <h1 className="text-[36px] text-white font-semibold">Get Playdate App <br /> Today!</h1>
-            <p className="text-[#ffffff] mt-4 text-[16px] font-light">Find Love and Adventure Anytime, Anywhere</p>
-            <div className="flex flex-row mt-2 space-x-3">
-              <Androids />
-              <Apples />
-            </div>
-            <p className="text-[#ffffff] mt-2 text-[16px] font-light">Find Love and Adventure Anytime, Anywhere</p>
+              <h1 className="text-[36px] text-white font-semibold">Get Playdate App <br/> Today!</h1> 
+             <p className="text-[#ffffff] mt-4 text-[16px] font-light">Find Love and Adventure Anytime, Anywhere</p> 
+             <div className="flex flex-row mt-2 space-x-3">
+                <Androids/>
+                <Apples/>
+             </div>
+             <p className="text-[#ffffff] mt-2 text-[16px] font-light">Find Love and Adventure Anytime, Anywhere</p> 
           </div>
-          {/* 02 */}
-          <div className=" ">
-            <Phone className="h-[533px] w-[630] -top-[92px] -right-3 relative" />
+           {/* 02 */}
+                <div className=" ">
+                <Phone className="h-[533px] w-[630] -top-[92px] -right-3 relative"/>   
           </div>
+        </div>   
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
 };
 export default SectionFive;
--- src/components/LandingPage/Sectiontwo.jsx
+++ src/components/LandingPage/Sectiontwo.jsx
@@ -1,44 +1,43 @@
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-import Image from "next/image";
-import people from  "../../../public/people.png"
+
 import Peeps from "../../../public/svgimage/People.svg"
 
 
 const Sectiontwo = () => {
-    return (
-      <main className={`mx-auto flex  bg-[#FFF9F6]   ${inter.className}`}>
-        {/* container */}
-        <div className="flex flex-row space-x-20 w-[1340px] h-[600px] mx-auto">
-          {/* 01 */}
-          <div className="w-[641px] ">
-            {/* <Image src={people} alt="people" height={500} width={500} /> */}
-            <Peeps/>
-          </div>
+  return (
+    <main className={`mx-auto flex  bg-[#FFF9F6]   ${inter.className}`}>
+      {/* container */}
+      <div className="flex flex-row space-x-20 w-[1340px] h-[600px] mx-auto">
+        {/* 01 */}
+        <div className="w-[641px] ">
+          {/* <Image src={people} alt="people" height={500} width={500} /> */}
+          <Peeps />
+        </div>
 
-          {/* 02 */}
-          <div className="max-w-[641px]">
-            <h1 className=" text-[36px] font-semibold">
-              Experience the Magic of Playdate <br /> Your Adventure in Love
-              Awaits
-            </h1>
-            <p className="text-[#303030] mt-4 text-[16px] font-light ">
-              PLAYDATE is more than a swipe. Connect with people who get you.
-              Whether <br /> it&apos;s a fun date or a lasting bond, your next
-              great experience starts here.
-            </p>
-            {/* <p className="text-[#303030] mt-4 text-[16px] font-light">
+        {/* 02 */}
+        <div className="max-w-[641px]">
+          <h1 className=" text-[36px] font-semibold">
+            Experience the Magic of Playdate <br /> Your Adventure in Love
+            Awaits
+          </h1>
+          <p className="text-[#303030] mt-4 text-[16px] font-light ">
+            PLAYDATE is more than a swipe. Connect with people who get you.
+            Whether <br /> it&apos;s a fun date or a lasting bond, your next
+            great experience starts here.
+          </p>
+          {/* <p className="text-[#303030] mt-4 text-[16px] font-light">
               Swipe, Connect, and Spark New Adventures with <br /> Like - Minded
               Singles Today!
             </p> */}
-            {/* btn */}
-            <div className="h-[54px] flex items-center justify-center w-[193px] rounded-[33px] bg-orange-gradient text-white mt-4">
-              <p> Join now </p>
-            </div>
+          {/* btn */}
+          <div className="h-[54px] flex items-center justify-center w-[193px] rounded-[33px] bg-orange-gradient text-white mt-4">
+            <p> Join now </p>
           </div>
         </div>
-      </main>
-    );
+      </div>
+    </main>
+  );
 };
 export default Sectiontwo;
